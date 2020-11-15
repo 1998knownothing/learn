5f53e0eb9d63eaa6602a2b2f65926ebf59231676
“liuye <1158982120@qq.com>
2020-11-15 20:06:47 +0800

    druidx整合starterOrNot

---
+++ druidx/src/main/java/com/example/druidx/DruidxApplication.java
@@ -0,0 +1,13 @@
+package com.example.druidx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidxApplication.class, args);
+    }
+
+}
